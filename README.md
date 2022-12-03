61a8b62405b07b5b9ed3486f88fe46b640275caf
Matthew Dangerfield <matthew.dangerfield2@gmail.com>
2022-12-03 11:56:13 -0700

    Field notes scanner

---
+++ README.md
@@ -0,0 +1,3 @@
+# Field Notes Scanner
+
+Uses google docs api to extract highlit text from a list of files shared with a service account. A poor man's version of something like [MAXQDA](https://www.maxqda.com/).
---
+++ index.js
@@ -0,0 +1,79 @@
+const { google } = require("googleapis");
+const fs = require("fs");
+
+const credentialFilename = "key.json";
+const scopes = [
+  "https://www.googleapis.com/auth/documents",
+  "https://www.googleapis.com/auth/documents.readonly",
+  "https://www.googleapis.com/auth/drive",
+  "https://www.googleapis.com/auth/drive.file",
+  "https://www.googleapis.com/auth/drive.readonly",
+];
+
+const auth = new google.auth.GoogleAuth({
+  keyFile: credentialFilename,
+  scopes: scopes,
+});
+const drive = google.drive({ version: "v3", auth });
+const docs = google.docs({ version: "v1", auth });
+
+function getFieldNoteFiles() {
+  return new Promise((resolve, reject) => {
+    drive.files.list(
+      {
+        pageSize: 1000,
+        fields: "nextPageToken, files(id, name)",
+      },
+      (err, res) => {
+        if (err) return reject("The API returned an error: " + err);
+        if (res.data.nextPageToken) return reject("Too many!");
+        resolve(
+          res.data.files.filter((file) => {
+            const name = file.name.toLowerCase();
+            return name.includes("field") && name.includes("notes");
+          })
+        );
+      }
+    );
+  });
+}
+
+async function extractHighlights(docId) {
+  const { data } = await docs.documents.get({ documentId: docId });
+  return data.body.content
+    .filter((segment) => segment.paragraph?.elements)
+    .flatMap((segment) => segment?.paragraph?.elements)
+    .filter(
+      (element) => element?.textRun?.textStyle?.backgroundColor?.color?.rgbColor
+    )
+    .map(({ textRun }) => ({
+      text: textRun.content,
+      color: simplifyColor(textRun.textStyle.backgroundColor.color.rgbColor),
+    }));
+}
+
+function simplifyColor(color) {
+  return `rgb(${color.red || 0}, ${color.green || 0}, ${color.blue || 0})`;
+}
+
+async function main() {
+  console.log("Listing files...");
+  const fieldNotes = await getFieldNoteFiles();
+
+  const allHighlights = [];
+  for (const fieldNote of fieldNotes) {
+    try {
+      console.log("Scanning", fieldNote.name);
+      allHighlights.push({
+        docId: fieldNote.id,
+        docName: fieldNote.name,
+        highlights: await extractHighlights(fieldNote.id),
+      });
+    } catch (e) {
+      console.error("Failed on ", fieldNote.id, e);
+    }
+  }
+
+  fs.writeFileSync("out.json", JSON.stringify(allHighlights, null, 2));
+}
+main();
